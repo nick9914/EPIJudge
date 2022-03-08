d7a8d00db74c8b51c0a3d552a395f89c60b5bc40
nvaldez6 <nvaldez6@bloomberg.net>
2022-03-08 09:00:26 -0500

    hanoi

--- epi_judge_cpp/hanoi.cc
+++ epi_judge_cpp/hanoi.cc
@@ -11,13 +11,6 @@ using std::stack;
 using std::vector;
 const int kNumPegs = 3;
 
-vector<vector<int>> initializePegs(int num_rings) {
-  vector<vector<int>> pegs(3);
-  for(int i = num_rings; i >= 1; i--) {
-    pegs[0].push_back(i);
-  }
-  return pegs;
-}
 // TODO: AHHH what where we thinking pass by value duh!
 void computeTowerHanoiAux(vector<vector<int>> & pegs, int num_rings, int from_peg, int to_peg, int using_peg, vector<vector<int>> & result) {
   if(num_rings > 0) {
@@ -31,11 +24,29 @@ void computeTowerHanoiAux(vector<vector<int>> & pegs, int num_rings, int from_pe
   }
 }
 
+void ComputeTowerHanoiSteps(int num_rings_to_move, 
+                            array<stack<int>, kNumPegs>& pegs, int from_peg, 
+                            int to_peg, int using_peg,
+                            vector<vector<int>>* result) {
+  if(num_rings_to_move > 0) {
+    ComputeTowerHanoiSteps(num_rings_to_move - 1, pegs, from_peg, using_peg, to_peg, result);
+    pegs[to_peg].push(pegs[from_peg].top());
+    pegs[from_peg].pop();
+    result -> emplace_back(vector<int>{from_peg, to_peg});
+    ComputeTowerHanoiSteps(num_rings_to_move - 1, pegs, using_peg, to_peg, from_peg, result);
+  }
+}
+
 vector<vector<int>> ComputeTowerHanoi(int num_rings) {
   // TODO - you fill in here.
-  vector<vector<int>> pegs = initializePegs(num_rings);
+  array<stack<int>, kNumPegs> pegs; //TODO How is this different and better than original 
+  // Initialize pegs
+  for(int i = num_rings; i >= 1; --i) {
+    pegs[0].push(i);
+  }
+
   vector<vector<int>> result;
-  computeTowerHanoiAux(pegs, num_rings, 0, 1, 2, result);
+  ComputeTowerHanoiSteps(num_rings, pegs, 0, 1, 2, &result);
   return result;
 }
 
